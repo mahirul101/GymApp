f5b61cf8cce1f231d167ddff6f7d49089e21a254
Sayem <83617577+SlayemS@users.noreply.github.com>
2024-01-21 01:49:01 -0500

    Added google maps api

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import { StatusBar } from 'expo-status-bar';
-import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
-import AppStack from './frontend/src/navigation/AppStack';
-import AppNav from './frontend/src/navigation/AppNav';
+import { StatusBar } from "expo-status-bar";
+import { TouchableOpacity, StyleSheet, Text, View } from "react-native";
+import AppStack from "./frontend/src/navigation/AppStack";
+import AppNav from "./frontend/src/navigation/AppNav";
 
 export default function App() {
-  return (
-    <AppNav />
-  );
+  return <AppNav />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- frontend/src/pages/SessionInfo.js
+++ frontend/src/pages/SessionInfo.js
@@ -1,113 +1,163 @@
 import {
-    View,
-    Text,
-    TextInput,
-    StatusBar,
-    TouchableOpacity,
-  } from "react-native";
-  import React, { useContext } from "react";
-  import SelectList from 'react-native-dropdown-select-list';
-  import { useNavigation } from "@react-navigation/native";
-  
-  function SessionInfo() {
-    const [WorkoutType, setWorkoutType] = React.useState("");
-    const [Location, setLocation] = React.useState("");
-    const [Date, setDate] = React.useState("");
-    const [Time, setTime] = React.useState("");
+  View,
+  Text,
+  TextInput,
+  StatusBar,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
+import SelectList from "react-native-dropdown-select-list";
+import { useNavigation } from "@react-navigation/native";
+import RNPickerSelect from "react-native-picker-select";
+import axios from "axios";
+import Config from "react-native-config";
 
-    const [selected, setSelected] = React.useState("");
+function SessionInfo() {
+  const apiKey = Config.GOOGLEMAPS_API_KEY;
 
-    const data = [
-        {key:'1',value:'Cardio'},
-        {key:'2',value:'Legs'},
-        {key:'3',value:'Arms'},
-        {key:'4',value:'Chest'},
-        {key:'5',value:'Back'},
-        {key:'6',value:'Shoulders'},        
-    ];
-  
-    const navigation = useNavigation();
-  
-    const SessionInfo = () => {
-      if (WorkoutType === "") {
-        alert("Please select your workout type");
-        return;
-      } else if (Location === "") {
-        alert("Please enter the location of the workout");
-        return;
-      } else if (Date === "") {
-        alert("Please enter the date of the workout");
-        return;
-      } else if (Time === "") {
-        alert("Please set the time of your workout");
-        return;
+  const [WorkoutType, setWorkoutType] = useState("");
+  const [Location, setLocation] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [Date, setDate] = useState("");
+  const [Time, setTime] = useState("");
+
+  const [selected, setSelected] = useState("");
+
+  const navigation = useNavigation();
+
+  const SessionInfo = () => {
+    if (WorkoutType === "") {
+      alert("Please select your workout type");
+      return;
+    } else if (Location === "") {
+      alert("Please enter the location of the workout");
+      return;
+    } else if (Date === "") {
+      alert("Please enter the date of the workout");
+      return;
+    } else if (Time === "") {
+      alert("Please set the time of your workout");
+      return;
+    }
+
+    navigation.navigate("SessionStack");
+  };
+
+  const handleLocationChange = async (text) => {
+    setLocation(text);
+    if (text.length > 2) {
+      // Trigger API call after 2 characters
+      try {
+        const response = await axios.get(
+          `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${text}&key=${Config.GOOGLEMAPS_API_KEY}`
+        );
+        setSuggestions(response.data.predictions);
+      } catch (error) {
+        console.error(error);
       }
-  
-      navigation.navigate("SessionStack");
-    };
+    } else {
+      setSuggestions([]);
+    }
+  };
 
-    return (
-      <View className="bg-white h-full w-full">
-        <StatusBar style="light" />
-  
-        <View className="flex-1 justify-around pt-40 pb-10">
-          <View className="items-center">
-            <Text className="text-5xl font-bold">Session Info</Text>
+  const handleSuggestionPress = (suggestion) => {
+    setLocation(suggestion.description); // Set the clicked suggestion as the location
+    setSuggestions([]); // Clear suggestions after selection
+  };
+
+  return (
+    <View className="bg-white h-full w-full">
+      <StatusBar style="light" />
+
+      <View className="flex-1 justify-around pt-40 pb-10">
+        <View className="items-center">
+          <Text className="text-5xl font-bold">Session Info</Text>
+        </View>
+
+        <View className="mx-4 space-y-4">
+          <View className="bg-gray-200 p-5 rounded-2xl w-full">
+            <RNPickerSelect
+              onValueChange={(value) => setWorkoutType(value)}
+              items={[
+                { label: "Cardio", value: "cardio" },
+                { label: "Arms", value: "arms" },
+                { label: "Chest", value: "chest" },
+                { label: "Back", value: "back" },
+                { label: "Shoulders", value: "shoulders" },
+                { label: "Legs", value: "legs" },
+                { label: "Upper Body", value: "upperbody" },
+                { label: "Lower Body", value: "lowerbody" },
+              ]}
+              placeholder={{
+                label: "Select a workout type...",
+                value: null,
+              }}
+              style={{ color: "black" }}
+            />
           </View>
-  
-          <View className="mx-4 space-y-4">
-            <View className="bg-gray-200 p-5 rounded-2xl w-full">
-              <TextInput
-                placeholder="Workout Type"
-                placeholderTextColor="gray"
-                value={WorkoutType}
-                onTextInput={setWorkoutType}
-                className="text-black" // Adjust the text color to ensure visibility
-              />
-            </View>
-            <View className="bg-gray-200 p-5 rounded-2xl w-full">
-              <TextInput
-                placeholder="Location"
-                value={Location}
-                onTextInput={setLocation}
-                placeholderTextColor="gray"
-                className="text-black" // Adjust the text color to ensure visibility
-              />
-            </View>
-            <View className="bg-gray-200 p-5 rounded-2xl w-full">
-              <TextInput
-                placeholder="Date"
-                value={Date}
-                onTextInput={setDate}
-                placeholderTextColor="gray"
-                className="text-black" // Adjust the text color to ensure visibility
-              />
-            </View>
-            <View className="bg-gray-200 p-5 rounded-2xl w-full mb-10">
-              <TextInput
-                placeholder="Time"
-                value={Time}
-                onTextInput={setTime}
-                placeholderTextColor="gray"
-                className="text-black" // Adjust the text color to ensure visibility
-                secureTextEntry
-              />
-            </View>
-            <View className="w-full">
-              <TouchableOpacity
-                onPress={SessionInfo}
-                className="w-full bg-red-700 p-3 rounded-2xl mb-3"
-              >
-                <Text className="text-center text-white font-bold">Create Session</Text>
-              </TouchableOpacity>
-            </View>
-            
-            
+          <View className="bg-gray-200 p-5 rounded-2xl w-full">
+            <TextInput
+              placeholder="Location"
+              value={Location}
+              onChangeText={handleLocationChange}
+              placeholderTextColor="gray"
+              style={{ color: "black" }}
+            />
+            {suggestions.length > 0 && (
+              <ScrollView style={{ backgroundColor: "#fff", maxHeight: 200 }}>
+                {suggestions.map((suggestion, index) => (
+                  <TouchableOpacity
+                    key={index}
+                    onPress={() => handleSuggestionPress(suggestion)}
+                    style={{
+                      paddingVertical: 10,
+                      paddingHorizontal: 10,
+                      borderBottomWidth: 1,
+                      borderBottomColor: "#ddd",
+                    }}
+                  >
+                    <Text style={{ borderRadius: 10 }}>
+                      {suggestion.description}
+                    </Text>
+                  </TouchableOpacity>
+                ))}
+              </ScrollView>
+            )}
+          </View>
+          <View className="bg-gray-200 p-5 rounded-2xl w-full">
+            <TextInput
+              placeholder="Date"
+              value={Date}
+              onTextInput={setDate}
+              placeholderTextColor="gray"
+              className="text-black" // Adjust the text color to ensure visibility
+            />
+          </View>
+          <View className="bg-gray-200 p-5 rounded-2xl w-full mb-10">
+            <TextInput
+              placeholder="Time"
+              value={Time}
+              onTextInput={setTime}
+              placeholderTextColor="gray"
+              className="text-black" // Adjust the text color to ensure visibility
+              secureTextEntry
+            />
+          </View>
+          <View className="w-full">
+            <TouchableOpacity
+              onPress={SessionInfo}
+              className="w-full bg-red-700 p-3 rounded-2xl mb-3"
+            >
+              <Text className="text-center text-white font-bold">
+                Create Session
+              </Text>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
-    );
-  }
-  
-  export default SessionInfo;
-  
+    </View>
+  );
+}
+
+export default SessionInfo;
