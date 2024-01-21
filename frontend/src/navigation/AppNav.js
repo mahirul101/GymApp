1b6ad2d08580c660e83457950528372639fb0910
Sayem <83617577+SlayemS@users.noreply.github.com>
2024-01-20 22:27:05 -0500

    Modified margin

--- frontend/src/navigation/AppNav.js
+++ frontend/src/navigation/AppNav.js
@@ -1,22 +1,24 @@
-import React, { useContext } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import LoginStack from './LoginStack';
-import AppStack from './AppStack';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import LoginStack from "./LoginStack";
+import AppStack from "./AppStack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNav() {
-    // let initialRouteName = 'LoginStack';
-    // if (true) {
-    //     initialRouteName = 'AppStack';
-    // }
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName='AppStack' screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="LoginStack" component={LoginStack} />
-                <Stack.Screen name="AppStack" component={AppStack} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
-};
+  let initialRouteName = "LoginStack";
+  if (true) {
+    initialRouteName = "AppStack";
+  }
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="AppStack"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="LoginStack" component={LoginStack} />
+        <Stack.Screen name="AppStack" component={AppStack} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
--- frontend/src/pages/Calendar.js
+++ frontend/src/pages/Calendar.js
@@ -1,267 +1,282 @@
-import { View, Text, SafeAreaView, StyleSheet, TouchableWithoutFeedback, TouchableOpacity, ScrollView } from "react-native"
-import React from 'react'
-import moment from 'moment'
-import Swiper from 'react-native-swiper'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React from "react";
+import moment from "moment";
+import Swiper from "react-native-swiper";
 import CalendarSession from "../components/CalendarSession";
 
-
 export default function Dev() {
-    const swiper = React.useRef();
-    const [value, setValue] = React.useState(new Date());
-    const [week, setWeek] = React.useState(0);
+  const swiper = React.useRef();
+  const [value, setValue] = React.useState(new Date());
+  const [week, setWeek] = React.useState(0);
 
-    const weeks = React.useMemo(() => {
-        const start = moment(start).add(weeks, 'weeks').startOf('week');
+  const weeks = React.useMemo(() => {
+    const start = moment(start).add(weeks, "weeks").startOf("week");
 
+    return [-1, 0, 1].map((adj) => {
+      return Array.from({ length: 7 }).map((_, index) => {
+        const date = moment(start).add(adj, "week").add(index, "day");
 
-        return [-1, 0, 1].map(adj => {
-            return Array.from({ length: 7 }).map((_, index) => {
-                const date = moment(start).add(adj, 'week').add(index, 'day')
+        return {
+          weekday: date.format("ddd"),
+          date: date.toDate(),
+        };
+      });
+    });
+  }, [week]);
 
-                return {
-                    weekday: date.format('ddd'),
-                    date: date.toDate(),
-                };
-            });
-        });
-    }, [week]);
+  const calendarSessions = [
+    {
+      workoutName: "Leg Day",
+      date: "14 January 2024",
+      time: "8:30-10:30",
+      // Add more properties here if needed
+    },
+    {
+      workoutName: "Leg Day",
+      date: "14 January 2024",
+      time: "8:30-10:30",
+      // Add more properties here if needed
+    },
+    {
+      workoutName: "Leg Day",
+      date: "14 January 2024",
+      time: "8:30-10:30",
+      // Add more properties here if needed
+    },
+    {
+      workoutName: "Leg Day",
+      date: "14 January 2024",
+      time: "8:30-10:30",
+      // Add more properties here if needed
+    },
+    // Add more sessions here
+  ];
 
-    const calendarSessions = [
-        {
-            workoutName: 'Leg Day',
-            date: '14 January 2024',
-            time: '8:30-10:30',
-            // Add more properties here if needed
-        },
-        {
-            workoutName: 'Leg Day',
-            date: '14 January 2024',
-            time: '8:30-10:30',
-            // Add more properties here if needed
-        },
-        {
-            workoutName: 'Leg Day',
-            date: '14 January 2024',
-            time: '8:30-10:30',
-            // Add more properties here if needed
-        },
-        // Add more sessions here
-    ];
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>Timetable</Text>
+        </View>
 
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <View style={styles.container}>
-                <View style={styles.header}>
-                    <Text style={styles.title}>Timetable</Text>
-                </View>
+        <View style={styles.picker}>
+          <Swiper
+            index={1}
+            ref={swiper}
+            showsPagination={false}
+            loop={false}
+            onIndexChanged={(index) => {
+              if (index === 1) {
+                return;
+              }
 
-                <View style={styles.picker}>
-                    <Swiper
-                        index={1} ref={swiper} showsPagination={false} loop={false}
-                        onIndexChanged={(index) => {
-                            if (index === 1) {
-                                return
-                            }
+              setTimeout(() => {
+                const newIndex = index - 1;
+                const newWeek = week + newIndex;
+                setWeek(newWeek);
+                setValue(moment(value).add(newIndex, "week").toDate());
+                swiper.current.scrollTo(1, false);
+              }, 100);
+            }}
+          >
+            {weeks.map((dates, index) => (
+              <View
+                style={[styles.itemRow, { paddingHorizontal: 16 }]}
+                key={index}
+              >
+                {dates.map((item, dateIndex) => {
+                  const isActive =
+                    value.toDateString() === item.date.toDateString();
+                  return (
+                    <TouchableWithoutFeedback
+                      key={dateIndex}
+                      onPress={() => setValue(item.date)}
+                    >
+                      <View
+                        style={[
+                          styles.item,
+                          isActive && {
+                            borderColor: "#111",
+                          },
+                        ]}
+                      >
+                        <Text
+                          style={[
+                            styles.itemWeekday,
+                            isActive && {
+                              color: "#111",
+                            },
+                          ]}
+                        >
+                          {item.weekday}
+                        </Text>
 
-                            setTimeout(() => {
-                                const newIndex = index - 1
-                                const newWeek = week + newIndex
-                                setWeek(newWeek);
-                                setValue(moment(value).add(newIndex, 'week').toDate());
-                                swiper.current.scrollTo(1, false);
-                            }, 100);
-                        }}>
+                        <Text
+                          style={[
+                            styles.itemDate,
+                            isActive && {
+                              color: "#111",
+                            },
+                          ]}
+                        >
+                          {item.date.getDate()}
+                        </Text>
+                      </View>
+                    </TouchableWithoutFeedback>
+                  );
+                })}
+              </View>
+            ))}
+          </Swiper>
+        </View>
 
-                        {weeks.map((dates, index) => (
-                            <View
-                                style={[styles.itemRow, { paddingHorizontal: 16 }]}
-                                key={index}>
-                                {dates.map((item, dateIndex) => {
-                                    const isActive =
-                                        value.toDateString() === item.date.toDateString();
-                                    return (
-                                        <TouchableWithoutFeedback
-                                            key={dateIndex}
-                                            onPress={() => setValue(item.date)}>
-                                            <View
-                                                style={[
-                                                    styles.item,
-                                                    isActive && {
-                                                        borderColor: "#111"
-                                                    },
-                                                ]}>
-                                                <Text
-                                                    style={[
-                                                        styles.itemWeekday,
-                                                        isActive && {
-                                                            color: "#111",
-                                                        },
-                                                    ]}>
-                                                    {item.weekday}
-                                                </Text>
+        <View style={{ flex: 8, paddingVertical: 10, paddingHorizontal: 10 }}>
+          <Text style={styles.contentText}>{value.toDateString()}</Text>
 
-                                                <Text
-                                                    style={[
-                                                        styles.itemDate,
-                                                        isActive && {
-                                                            color: "#111",
-                                                        },
-                                                    ]}
-                                                >
-                                                    {item.date.getDate()}
-                                                </Text>
-                                            </View>
-                                        </TouchableWithoutFeedback>
-                                    );
-                                })}
+          <View style={styles.placeholder}>
+            <ScrollView>
+              <View style={styles.placeholderContent}>
+                {calendarSessions.map((session, index) => (
+                  <CalendarSession
+                    key={index}
+                    workoutName={session.workoutName}
+                    date={session.date}
+                    time={session.time}
+                    // Pass more props here if needed
+                  />
+                ))}
+              </View>
+            </ScrollView>
+          </View>
 
-                            </View>
-                        ))}
-                    </Swiper>
-                </View>
-
-                <View style={{ flex: 8, paddingVertical: 10, paddingHorizontal: 10 }}>
-                    <Text style={styles.contentText}>{value.toDateString()}</Text>
-
-
-                    <View style={styles.placeholder}>
-                        <ScrollView>
-                            <View style={styles.placeholderContent}>
-                                {calendarSessions.map((session, index) => (
-                                    <CalendarSession
-                                        key={index}
-                                        workoutName={session.workoutName}
-                                        date={session.date}
-                                        time={session.time}
-                                    // Pass more props here if needed
-                                    />
-                                ))}
-                            </View>
-                        </ScrollView>
-                    </View>
-
-                    <View style={styles.footer}>
-                        <TouchableOpacity style={styles.btn} onPress={() => {
-                            //handle create session 
-                        }}>
-                            <Text style={styles.btnText}>Create Session</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-
-            </View>
-        </SafeAreaView>
-    );
+          <View style={styles.footer}>
+            <TouchableOpacity
+              style={styles.btn}
+              onPress={() => {
+                //handle create session
+              }}
+            >
+              <Text style={styles.btnText}>Create Session</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingVertical: 24,
-    },
+  container: {
+    flex: 1,
+    paddingVertical: 24,
+  },
 
-    picker: {
-        flex: 1,
-        maxHeight: '74',
-        paddingVertical: 12,
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
+  picker: {
+    flex: 1,
+    maxHeight: "74",
+    paddingVertical: 12,
+    flexDirection: "row",
+    alignItems: "center",
+  },
 
-    header: {
-        paddingHorizontal: 16,
-    },
+  header: {
+    paddingHorizontal: 16,
+  },
 
-    contentText: {
-        fontSize: 17,
-        fontWeight: '600',
-        color: '#333',
-        marginBottom: 12,
-    },
+  contentText: {
+    fontSize: 17,
+    fontWeight: "600",
+    color: "#333",
+    marginBottom: 12,
+  },
 
-    title: {
-        fontSize: 32,
-        fontWeight: 'bold',
-        color: '#333',
-        marginBottom: 12,
-    },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 12,
+  },
 
-    itemRow: {
-        width: '1',
-        flexDirection: 'row',
-        alignItems: 'flex-start',
-        justifyContent: 'space-between',
-        marginHorizontal: 10,
+  itemRow: {
+    width: "1",
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+    marginHorizontal: 10,
+  },
 
-    },
+  item: {
+    flex: 1,
+    height: 50,
+    marginHorizontal: 4,
+    paddingVertical: 10,
+    paddingHorizontal: 4,
+    borderWidth: 1,
+    borderColor: "#e3e3e3",
+    borderRadius: 8,
+    alignItems: "center",
+    flexDirection: "column",
+  },
 
-    item: {
-        flex: 1,
-        height: 50,
-        marginHorizontal: 4,
-        paddingVertical: 10,
-        paddingHorizontal: 4,
-        borderWidth: 1,
-        borderColor: '#e3e3e3',
-        borderRadius: 8,
-        alignItems: 'center',
-        flexDirection: 'column',
-    },
+  itemWeekday: {
+    fontSize: 13,
+    fontWeight: "bold",
+  },
 
-    itemWeekday: {
-        fontSize: 13,
-        fontWeight: 'bold',
-    },
+  itemDate: {
+    fontSize: 13,
+    fontWeight: "bold",
+  },
 
-    itemDate: {
-        fontSize: 13,
-        fontWeight: 'bold',
-    },
+  placeholder: {
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+    height: 400,
+    marginTop: 0,
+    padding: 0,
+    backgroundColor: "transparent",
+  },
 
-    placeholder: {
-        flexGrow: 1,
-        flexShrink: 1,
-        flexBasis: 0,
-        height: 400,
-        marginTop: 0,
-        padding: 0,
-        backgroundColor: 'transparent',
-    },
+  placeholderContent: {
+    borderWidth: 4,
+    borderColor: "#e5e7eb",
+    borderStyle: "dashed",
+    borderRadius: 9,
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+    padding: 10,
+  },
 
-    placeholderContent: {
-        borderWidth: 4,
-        borderColor: '#e5e7eb',
-        borderStyle: 'dashed',
-        borderRadius: 9,
-        flexGrow: 1,
-        flexShrink: 1,
-        flexBasis: 0,
-        padding: 10,
+  footer: {
+    marginTop: 24,
+    paddingHorizontal: 16,
+  },
 
-    },
-
-    footer: {
-        marginTop: 24,
-        paddingHorizontal: 16,
-    },
-
-    /** Button */
-    btn: {
-        flexDirection: 'row',
-        backgroundColor: '#007aff',
-        borderWidth: 1,
-        borderColor: '#007aff',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 8,
-        alignItems: 'center',
-        justifyContent: 'center',
-
-    },
-    btnText: {
-        fontSize: 18,
-        fontWeight: '600',
-        color: '#fff',
-    },
+  /** Button */
+  btn: {
+    flexDirection: "row",
+    backgroundColor: "#007aff",
+    borderWidth: 1,
+    borderColor: "#007aff",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btnText: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#fff",
+  },
 });
