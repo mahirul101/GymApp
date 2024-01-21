6e17344983dd4cf6f6afeeb7db01e7b6af00b00e
mahirul101 <mahirul.islam@mail.mcgill.ca>
2024-01-20 20:00:13 -0500

    sessions page

--- frontend/src/pages/Sessions.js
+++ frontend/src/pages/Sessions.js
@@ -1,8 +1,76 @@
-import { View, Text } from "react-native"
+import { View, Text, StyleSheet, Dimensions, ScrollView } from "react-native"
+import SessionCard from "../components/SessionCard";
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 export default function Dev() {
 
-    return (<View>
-        <Text>Dev</Text>
-    </View>)
+    const sessions = [
+        {
+            workoutName: 'Leg Day',
+            date: '14 January 2024',
+            time: '5:15PM',
+            location: 'Buzzfit DDO',
+            user: 'sayem',
+        },
+        {
+            workoutName: 'Leg Day',
+            date: '14 January 2024',
+            time: '5:15PM',
+            location: 'Buzzfit DDO',
+            user: 'sayem',
+        },
+    ];
+
+    return (
+        <View>
+            <View style={styles.container}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', }}>
+                    <Text style={{ fontSize: 20, marginLeft: 10, marginBottom: 20 }}>Upcoming Sessions</Text>
+                    <AntDesign name='filter' size={20} right={20}></AntDesign>
+                </View>
+                <ScrollView>
+                    <View style={styles.allSessions}>
+                        {sessions.map((session, index) => (
+                            <SessionCard
+                                key={index}
+                                workoutName={session.workoutName}
+                                date={session.date}
+                                time={session.time}
+                                location={session.location}
+                                user={session.user}
+                            />
+                        ))}
+                    </View>
+                </ScrollView>
+            </View>
+        </View >)
 }
+
+const deviceWidth = Math.round(Dimensions.get('window').width);
+const deviceHeight = Math.round(Dimensions.get('window').height);
+const offset = 40;
+const radius = 20;
+const styles = StyleSheet.create({
+    container: {
+        width: deviceWidth,
+        // alignItems: 'center',
+        marginTop: 25,
+        border: 'solid',
+    },
+    cardTitle: {
+        fontSize: 20,
+        marginLeft: 10,
+        marginTop: 10,
+    },
+    cardText: {
+        fontSize: 15,
+        marginLeft: 10,
+        marginTop: 10,
+    },
+    allSessions: {
+        borderTopWidth: 1,
+        marginHorizontal: 20,
+        borderColor: '#c2c5cc',
+        height: deviceHeight,
+    }
+});
